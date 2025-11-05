a2b8d0f69c3b80884f84fb994eca47d886006861
Mirza Arnaut <mirza.arnaut45@gmail.com>
2025-11-05 17:12:43 +0100

    chore: finilize M02

--- exercise02/M02.py
+++ exercise02/M02.py
@@ -1,17 +1,38 @@
 import numpy as np
+import numpy.typing as npt
 import streamlit as st
 from plotly import graph_objects as go
+from scipy.fft import fft, fftfreq
 
-sampling_frequency = st.slider("Sampling Frequency", 10.0, 5000.0, 2000.0, 10.0)
-start_time = st.slider("Start Time", 0.0, 10.0, 0.0, 0.1)
-stop_time = st.slider("Stop Time", 0.1, 10.0, 5.0, 0.1)
 
-time = np.arange(start_time, stop_time, 1 / sampling_frequency)
+def sample(
+    signal: npt.NDArray,
+    sampling_frequency: float,
+    new_sampling_frequency: float,
+    # hold: bool = False,
+):
+    if new_sampling_frequency > sampling_frequency:
+        raise Exception("The new sampling frequency can't be larger than the old one!")
+
+    sampling_step = int(sampling_frequency // new_sampling_frequency)
+    return signal[::sampling_step]
+
+
+col1, col2, col3, col4 = st.columns(4)
+sampling_frequency = col1.number_input("Sampling Frequency", 10.0, 5000.0, 2000.0, 10.0)
+start_time = col2.number_input("Start Time", 0.0, 9.0, 0.0, 1.0)
+end_time = col3.number_input("Stop Time", 0.001, 10.0, 5.0, 1.0)
+subsampling_factor = col4.number_input("Sub-Sampling Factor", 1, 1000, 10, 1)
+if start_time >= end_time:
+    st.error("Start time has to be lower than end time")
+    st.stop()
+st.divider()
+
+time = np.arange(start_time, end_time, 1 / sampling_frequency)
 # or
-# number_sampling_points = sampling_frequency * (stop_time - start_time)
-# time = np.linspace(start_time, stop_time, number_sampling_points)
+# number_sampling_points = sampling_frequency * (end_time - start_time)
+# time = np.linspace(start_time, end_time, number_sampling_points)
 
-subsampling_factor = 10
 subsampling_frequancy = sampling_frequency / subsampling_factor
 
 # amplitude = np.array([1, 1.2, 0.6])
@@ -34,15 +55,16 @@ with col2:
     )
 with col3:
     phases = [np.pi * x for x in [0, 1 / 6, 1 / 4, 1 / 3, 1 / 2, 1]]
-    format_func = lambda x: rf"pi/{(x / np.pi) ** -1 if x > 0 else 0}"
+
+    def format_func(x):
+        return f"pi/{(x / np.pi) ** -1 if x > 0 else 0}"
+
     end = len(phases) - 1
     phase = np.array(
         [
             st.selectbox("Phase 1", phases, format_func=format_func, index=0),
             st.selectbox("Phase 2", phases, format_func=format_func, index=end),
             st.selectbox("Phase 3", phases, format_func=format_func, index=4),
-            # st.slider("Phase 2", 0.0, 2 * np.pi, 3 * np.pi, 0.1),
-            # st.slider("Phase 3", 0.0, 2 * np.pi, np.pi / 2, 0.1),
         ]
     )
 signal = np.sum(
@@ -52,17 +74,76 @@ signal = np.sum(
     ],
     axis=0,
 )
-#     np.sin(2 * np.pi * time)
-#     + 1.2 * np.sin(2 * np.pi * 12 * time + 3 * np.pi)
-#     + 0.6 * np.cos(2 * np.pi * 45 * time)
 
-# st.latex(rf"\sin(2\pi{1}) + 1.2 \cdot \sin(2\pi{})")
+subsampling_frequancy = sampling_frequency / subsampling_factor
+sampled_signal = sample(signal, sampling_frequency, subsampling_frequancy)
+sampled_time = np.linspace(start_time, end_time, sampled_signal.size)
 
 fig = go.Figure()
 fig.add_trace(
     go.Scatter(
+        x=sampled_time,
+        y=sampled_signal,
+        name="Sampled",
+    )
+)
+fig.add_trace(
+    go.Scatter(
         x=time,
         y=signal,
+        name="Original",
+        line=dict(color="rgba(255, 0, 0, 0.25)"),
     )
+)
+fig.update_layout(
+    title="Signal",
+    xaxis=dict(title="Time", ticksuffix="s"),
+    yaxis=dict(title="Signal"),
+)
+st.plotly_chart(fig)
+
+st.divider()
+st.info("Extra! See what happens when the sub-sampling factor goes beyond 20 ;)")
+
+spectrum = np.array(fft(signal)) / sampling_frequency
+frequency = fftfreq(spectrum.size, 1 / sampling_frequency)
+spectrum_sampled = np.array(fft(sampled_signal)) / subsampling_frequancy
+frequency_sampled = fftfreq(spectrum_sampled.size, 1 / subsampling_frequancy)
+
+col1, col2 = st.columns(2)
+half = col1.checkbox("Show positive spectrum only", value=True)
+cutoff_frequency = col2.number_input(
+    "Cutoff Frequency",
+    0.0,
+    float(sampling_frequency / 2),
+    float(sampling_frequency / 2),
+    1.0,
+)
+if half:
+    spectrum = spectrum[0 : len(spectrum) // 2]
+    frequency = frequency[0 : len(frequency) // 2]
+    spectrum_sampled = spectrum_sampled[0 : len(spectrum_sampled) // 2]
+    frequency_sampled = frequency_sampled[0 : len(frequency_sampled) // 2]
+
+fig = go.Figure()
+fig.add_trace(
+    go.Scatter(
+        x=frequency_sampled,
+        y=np.abs(spectrum_sampled),
+        name="Sampled",
+    )
+)
+fig.add_trace(
+    go.Scatter(
+        x=frequency,
+        y=np.abs(spectrum),
+        name="Original",
+        line=dict(color="rgba(255, 0, 0, 0.5)"),
+    )
+)
+fig.update_layout(
+    title="Spectrum",
+    xaxis=dict(title="Frequency", ticksuffix="Hz", range=[0, cutoff_frequency]),
+    yaxis=dict(title="Spectrum"),
 )
 st.plotly_chart(fig)
